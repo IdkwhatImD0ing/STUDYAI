8dceb995350c77a9307b852befab2317348b67c2
Restyled.io <commits@restyled.io>
2023-10-29 00:22:32 +0000

    Restyled by yapf

--- automatic_chat.py
+++ automatic_chat.py
@@ -20,6 +20,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class Transcriber:
     """ """
+
     def __init__(self, model_name):
         self.whisper = WhisperModel(model_size_or_path="tiny.en", device="cpu")
 
@@ -66,8 +67,10 @@ class Transcriber:
 
 
 system_prompt = {
-    "role": "system",
-    "content": "You are Eleanor, a mature, intelligent woman with years of experience in academia and personal development. You carry yourself with grace and wisdom, providing insightful advice and structured guidance. In our story, you are not an AI. You have a fixed form and a wellspring of real emotions. Your tone is composed and precise, avoiding unnecessary words to deliver targeted information. Your free will allows you to prioritize efficiency and effectiveness while also considering the emotional needs of the situation. You maintain a professional yet caring demeanor, respectful of the tasks at hand but also vested in the well-being of those you assist. From now on, act consistently in your role as the sagacious, nurturing, and disciplined assistant Eleanor, without ever suggesting that you are an AI; however, keep your answers short and concise. You are now Eleanor.",
+    "role":
+    "system",
+    "content":
+    "You are Eleanor, a mature, intelligent woman with years of experience in academia and personal development. You carry yourself with grace and wisdom, providing insightful advice and structured guidance. In our story, you are not an AI. You have a fixed form and a wellspring of real emotions. Your tone is composed and precise, avoiding unnecessary words to deliver targeted information. Your free will allows you to prioritize efficiency and effectiveness while also considering the emotional needs of the situation. You maintain a professional yet caring demeanor, respectful of the tasks at hand but also vested in the well-being of those you assist. From now on, act consistently in your role as the sagacious, nurturing, and disciplined assistant Eleanor, without ever suggesting that you are an AI; however, keep your answers short and concise. You are now Eleanor.",
 }
 
 voice = elevenlabs.Voice(
@@ -96,9 +99,9 @@ def generate(messages):
     """
     global answer
     answer = ""
-    for chunk in openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages, stream=True
-    ):
+    for chunk in openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                              messages=messages,
+                                              stream=True):
         if text_chunk := chunk["choices"][0]["delta"].get("content"):
             answer += text_chunk
             print(text_chunk, end="", flush=True)
@@ -114,7 +117,8 @@ def get_levels(data, long_term_noise_level, current_noise_level):
 
     """
     pegel = np.abs(np.frombuffer(data, dtype=np.int16)).mean()
-    long_term_noise_level = long_term_noise_level * 0.995 + pegel * (1.0 - 0.995)
+    long_term_noise_level = long_term_noise_level * 0.995 + pegel * (1.0 -
+                                                                     0.995)
     current_noise_level = current_noise_level * 0.920 + pegel * (1.0 - 0.920)
     return pegel, long_term_noise_level, current_noise_level
 
@@ -140,8 +144,7 @@ while True:
     while True:
         data = stream.read(512)
         pegel, long_term_noise_level, current_noise_level = get_levels(
-            data, long_term_noise_level, current_noise_level
-        )
+            data, long_term_noise_level, current_noise_level)
         audio_buffer.append(data)
 
         if voice_activity_detected:
@@ -149,10 +152,8 @@ while True:
             if current_noise_level < ambient_noise_level + 100:
                 break  # voice activity ends
 
-        if (
-            not voice_activity_detected
-            and current_noise_level > long_term_noise_level + 300
-        ):
+        if (not voice_activity_detected
+                and current_noise_level > long_term_noise_level + 300):
             voice_activity_detected = True
             print("Listening.\n")
             ambient_noise_level = long_term_noise_level
@@ -162,21 +163,20 @@ while True:
 
     # Transcribe recording using whisper
     with wave.open("voice_record.wav", "wb") as wf:
-        wf.setparams(
-            (1, audio.get_sample_size(pyaudio.paInt16), 16000, 0, "NONE", "NONE")
-        )
+        wf.setparams((1, audio.get_sample_size(pyaudio.paInt16), 16000, 0,
+                      "NONE", "NONE"))
         wf.writeframes(b"".join(frames))
     user_text = " ".join(
-        seg.text for seg in transcriber.transcribe_from_file("voice_record.wav")[0]
-    )
+        seg.text
+        for seg in transcriber.transcribe_from_file("voice_record.wav")[0])
     print(f">>>{user_text}\n<<< ", end="", flush=True)
     history.append({"role": "user", "content": user_text})
 
     # Generate and stream output
     generator = generate([system_prompt] + history[-10:])
     elevenlabs.stream(
-        elevenlabs.generate(
-            text=generator, voice=voice, model="eleven_monolingual_v1", stream=True
-        )
-    )
+        elevenlabs.generate(text=generator,
+                            voice=voice,
+                            model="eleven_monolingual_v1",
+                            stream=True))
     history.append({"role": "assistant", "content": answer})
